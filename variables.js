a131220998f725522b47fdfa477a1230d00b5bc3
newmimshen <mohammad.sharafi.1994.iranian@gmail.com>
2025-05-09 19:42:56 +0330

    check if git works perfectly

--- variables.js
+++ variables.js
@@ -32,3 +32,4 @@ const sunPersons = {
 
 const persons = ['nabi', 'ali', 'fatima', 'hasan', 'hussain', 'sajjad', 'bagir', 'sadigh', 'kazim', 'reza', 'javad', 'hadi', 'askari' ,'hojjat'];
 let time = new Date().getDay();
+
